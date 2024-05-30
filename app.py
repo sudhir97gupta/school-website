468b61ddd5d867d9e48318cf22194826018ad41b
sudhir97gupta <162624145+sudhir97gupta@users.noreply.github.com>
2024-05-30 06:04:47 +0000

    Add Css

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World"
+    return render_template('home.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
